8395f6bd3a763d8501627a88c763ae7a6b24487e
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-08-10 19:51:34 +0530

    Added one Hello Controller | secured this resource with default spring security username and password

---
+++ auth-service/src/main/java/org/security/auth_service/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.security.auth_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok("Hello World") ;
+    }
+}
