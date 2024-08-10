796c178373d744b549db15d820bf271d75c660c2
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-08-10 20:18:36 +0530

    Added HttpServletRequest Session ID | Added custom username and password only for Single user

--- auth-service/src/main/java/org/security/auth_service/controller/HelloController.java
+++ auth-service/src/main/java/org/security/auth_service/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.security.auth_service.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public ResponseEntity<String> helloWorld(){
-        return ResponseEntity.ok("Hello World") ;
+    public ResponseEntity<String> helloWorld(HttpServletRequest request){
+        return ResponseEntity.ok("Hello World " + request.getSession().getId()) ;
     }
 }
